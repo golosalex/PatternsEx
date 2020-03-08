3a5eb4b9b0ebb7df978812aee34c8a3505319521
golosalex <Golosalex@mail.ru>
2020-03-08 14:58:42 +0300

    начало проекта. заготовка для паттерна фасад

---
+++ structPatterns/Facade/FasadePatternConsole/FasadePatternConsole/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// пример написан на основе видио https://www.youtube.com/watch?v=ANlcc2p9kCU&list=PLwcDaxeEINactCC4mly7RQon5juIpH-Q3&index=3
+namespace FasadePatternConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Power pow = new Power();
+            pow.On();
+            DVDRom dvd = new DVDRom();
+            dvd.Load();
+
+            HDD hdd = new HDD();
+            hdd.CopyFromDVD(dvd);
+        }
+    }
+    
+    class Power
+    {
+        public void On()
+        {
+            Console.WriteLine("включение питания");
+
+        }
+        public void Off()
+        {
+            Console.WriteLine("Выключение питания");
+        }
+    }
+    class DVDRom
+    {
+        private bool data = false;
+        public bool Data
+        {
+            get
+            {
+                return data;
+            }
+            
+        }
+
+        public void Load()
+        {
+            data = true;
+        }
+
+        public void Unload()
+        {
+            data = false;
+        }
+    }
+
+    class HDD
+    {
+        public void CopyFromDVD(DVDRom dvd)
+        {
+            if (dvd.Data)
+            {
+                Console.WriteLine("Данные скопированы");
+            }
+            else
+            {
+                Console.WriteLine("Нет диска в дисководе");
+            }
+        }
+    }
+}
