9ee9c44afa6d0e89883eae6538239315e63d7f9c
golosalex <golosalex1>
2020-03-30 01:22:02 +0300

    паттерн адаптер

---
+++ structPatterns/Adaptor/ConsoleApp1/ConsoleApp1/AdapterForOldClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class AdapterForOldClass : IHelloClass
+    {
+        OldClass a = new OldClass();
+        public void HelloWord()
+        {
+            a.GoodOldMethod();
+        }
+    }
+}
