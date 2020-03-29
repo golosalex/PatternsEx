2f3c8542d5d8c1e05cdf249d3a80632a538b8d4e
golosalex <golosalex1>
2020-03-29 05:05:26 +0300

    выполнено дз многопоточность

---
+++ DZ/dzMod12/Maltythread/Maltythread/MyQueue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DAL;
+
+namespace Maltythread
+{
+    public class MyQueue<T> where T: class
+    {
+        private Node Head { get; set; }
+        private Node Tail { get; set; }
+
+        public int MaxVol { get; set; }
+        private volatile int _vol;
+        public int Vol
+        {
+            get
+            {
+                return _vol;
+            }
+        }
+
+        public MyQueue(int maxVol)
+        {
+            MaxVol = maxVol;
+            Node EmptyElement = new Node();
+            Head = Tail = EmptyElement; // в очереди всегда будет хотябы один пустой хлемент(Node.Data=null)
+                                        // критерий пустоты очереди при dequeue это head = tail = null
+        }
+
+        public void Enqueue(T data)
+        {
+            Tail.Next = new Node() { Data = data }; // добавленный элемент пока недоступен считывающему потоку
+            Interlocked.Increment(ref _vol); // атомарная операция инкремента чтобы Dequeue не испортил
+            if(_vol>MaxVol) Console.WriteLine($"был превышен объем очереди на примерно {_vol-MaxVol} элементов, но точно больше чем на 1");
+            //контроль максимального объема очереди надо проводить снаружи в вызывающем потоке
+            Tail = Tail.Next; // атомарная операция теперь элемент полностью добавлен и доступен.
+        }
+        public T Dequeue()
+        {
+            if (Head == Tail)// если голова и хвост совпали то указатель головы не двигать. очередь пуста
+            {
+                // по логике тут всегда должно быть  Head.Data=null если нет, то эксепшин.
+                if (Head.Data != null) throw new Exception("Dequeue uncorect result Loks like more then one thread can use Dequeue method");
+                return null; 
+            }
+            //случай когда head не равно tail. в голове остался пустой элемент с момента инициализации или с прошлого Dequeue
+            if (Head.Data == null)
+            {
+                Head = Head.Next;
+                T Templ = Head.Data;
+                Head.Data = null; //в голове остался пустой элемент
+                Interlocked.Decrement(ref _vol);
+                if (_vol < 0) throw new Exception("some problem with queue volume. it has negative value now");
+                return Templ;
+            }
+            //если почему-то оказалось что в head не null то эксепшен. но по логике он возникнуть не может.
+            throw new Exception("Loks like more then one thread can use Dequeue method");
+        }
+
+
+        private class Node
+        {
+            public Node Next { get; set; }
+            public T Data { get; set; }
+        }
+    }
+}
--- DZ/dzMod12/Maltythread/Maltythread/Program.cs
+++ DZ/dzMod12/Maltythread/Maltythread/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,45 +18,117 @@ namespace Maltythread
 
         static void Main(string[] args)
         {
+            const int allFileVol = 1000000;
+            const int max = 100000; // 100 000 персонов максимум в очереди: всего в фаил планируется залить 10 миллионов человек
+            Console.WriteLine("введите размер порции. экспериментально рекомендованно от 1000 до 5000");
+            Console.WriteLine("ожидаемое время записи 50 секунд два потока и 80 секунд 1 поток");
+            int portion = int.Parse(Console.ReadLine()); // получили размер порции например 1000
+            int lengthOfQueue = max / portion; // максимальная длина очереди
+            MyQueue<Person[]> queue = new MyQueue<Person[]>(lengthOfQueue);
 
-            int Max = 100000; // 100 000 персонов: всего в фаил планируется залить 10 миллионов человек
+            PersonsProvider provider = new PersonsProvider(); // создаем экземпляр, который будет генерировать порции для очереди
+            string directory = Directory.GetCurrentDirectory();
+            string fullname = Path.Combine(directory, "big1.txt");
+            Stopwatch timer=new Stopwatch();
 
+            timer.Start();
+            Thread thread1 = new Thread(() => AdderToQueue(queue, provider, allFileVol, portion));
+            thread1.Start();
 
-
-            int Partion = int.Parse(Console.ReadLine()); // получили размер порции например 1000
-            int LengthOfQueue = Max / Partion; // максимальная длина очереди
-
-            
-
-            PersonsProvider a = new PersonsProvider(); // создаем экземпляр, который будет генерировать порции для очереди
-
-            //Console.WriteLine(LengthOfQuery);
-            MyQueue queue = new MyQueue(LengthOfQueue); // создаем очередь ограниченной длинны
-
-            Thread WriteThread = new Thread(() => ReadMethod(queue));
-            WriteThread.Start(); // с этого момента 2 потока
-
-            while (true)
+            using (StreamWriter sw = new StreamWriter(fullname))
             {
-                if (queue.full)
+                Person[] persons;
+                int waitTime = 0;
+                int waitDelta = 200;
+                while (true)
                 {
-                    continue;
-                }
+                    //код для слежения за полнотой очереди.
+                    //Console.WriteLine($"полнота очереди{1.0*queue.Vol/queue.MaxVol*100}%");// полнота очереди
 
-                queue.push();
-                // прервать цикл когда будет 10 000 000 человек в текстовом файле.
+                    persons = queue.Dequeue();
+                    if (persons == null)
+                    {
+                        if (thread1.IsAlive)
+                        {
+                            waitTime += waitDelta;
+                            Console.WriteLine($"при запись в файл спали {waitTime}мс");
+                            Thread.Sleep(waitDelta);//на практике 200мс это примерное время заполнения очереди до 20%.
+                            continue;
+                        }
+                        else
+                        {
+                            if (queue.Vol>0) continue;
+                            break;
+                        }
+                    }
+
+                    foreach (var person in persons)
+                    {
+                        sw.WriteLine(person);
+                    }
+
+                    waitTime = 0;
+                }    
             }
-
-
-
-
-
+            timer.Stop();
+            Console.WriteLine($"запись в двухпоточном режиме {timer.ElapsedMilliseconds}");
+            
+            
+            //однопоточный вариант
+            Console.WriteLine("Запись в однопоточном режиме начата. примерное время ожидания в 1.5 больше чем у двухпоточного");
+            timer = new Stopwatch();
+            timer.Start();
+            fullname = Path.Combine(directory, "big2.txt");
+            using (StreamWriter sw = new StreamWriter(fullname))
+            {
+                
+                int index = 0;
+                while (index < allFileVol)
+                {
+                    Person[] p = provider.GetPersons(index + 1, portion);
+                    foreach (var person in p)
+                    {
+                        sw.WriteLine(person);
+                    }
+                    //queue.Enqueue(provider.GetPersons(index + 1, portion));
+                    index = index + portion;
+                } 
+            }
+            timer.Stop();
+            Console.WriteLine($"запись в однопоточном режиме {timer.ElapsedMilliseconds}");
+            
 
         }
 
-        private static void ReadMethod(MyQueue query)
+        private static void AdderToQueue(MyQueue<Person[]> queue, PersonsProvider provider, int allFileVol, int portion)
         {
-            throw new NotImplementedException();
+            int index = 0;
+            while (true)
+            {
+                if (index > allFileVol) throw new Exception("place extra person");
+                if (index==allFileVol) break;
+                if (index + portion > allFileVol) portion = allFileVol - index;
+
+                bool success = false;
+                for (int i = 0; i < 200; i++)
+                {
+                    if (queue.Vol >= queue.MaxVol)
+                    {
+                        Thread.Sleep(20);
+                        Console.WriteLine($"при добавлении в полную очередь спали примерно {(i+1)*20} милисекунд");
+                    }
+                    else
+                    {
+                        queue.Enqueue(provider.GetPersons(index+1,portion));
+                        index = index + portion;
+                        success = true;
+                        break;
+                    }
+                }
+                if(!success)throw new Exception("очередь полна более четырех секунд");
+
+
+            }
         }
     }
 }
